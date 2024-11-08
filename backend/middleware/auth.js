ded0343894f2b6b38eadbe0f505abb00f2a9381e
Jean-Pierre Sabah <jeanpierresabah@gmail.com>
2024-11-08 20:08:34 +0100

    suppression console.log

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,11 +3,9 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
   try {
-    console.log(req.headers.authorization);
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.SECRET);
     const userId = decodedToken.userId;
-    console.log(userId);
     req.auth = {
       userId: userId,
     };
